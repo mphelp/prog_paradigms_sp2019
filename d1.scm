3affd3d91016c14547fb187fb9038a06e9825d72
Matthew Phelps <mphelps3@student04.cse.nd.edu>
2019-01-17 02:28:16 -0500

    complete first hw

--- d1.scm
+++ d1.scm
@@ -14,11 +14,17 @@
 (display (atom? (car (cdr (cdr q)))))
 (display "\n")
 ;; output:
-;; (copy the output you saw here)
-;;
+;; question 1: #f
+
 ;; explanation:
-;; (use as many lines as necessary, just add more comments)
-;;
+;; -- The car of the cdr of the cdr of the list q is another list
+;;    i.e. (stuffing potatoes ham) and thus the function atom? returns FALSE
+;; -- Procedure in full:
+;; 			atom? (car (cdr (cdr (turkey (gravy) (stuffing potatoes ham) peas))))
+;; 			atom? (car (cdr ((gravy) (stuffing potatoes ham) peas)))
+;; 			atom? (car ((stuffing potatoes ham) peas))
+;; 			atom? ((stuffing potatoes ham))
+;;   		#f
 
 
 ;; question 2
@@ -26,10 +32,12 @@
 (display (lat? (car (cdr (cdr q)))))
 (display "\n")
 ;; output:
-;;
+;; question 2: #t
 ;;
 ;; explanation:
-;;
+;; -- The list above (stuffing potatoes ham) here is the result once again of car (cdr (cdr q))
+;;   	Rather than asking if the list is an atom, lat? asks if it is a list of atoms,
+;;    to which the correct value is returned: true i.e. #t
 ;;
 
 
@@ -38,10 +46,14 @@
 (display (cond ((atom? (car q)) (car q)) (else '())))
 (display "\n")
 ;; output:
-;;
+;; question 3: turkey
 ;;
 ;; explanation:
 ;; NOTE, in your explanation, be sure to explain what 'cond' and 'else' are.
-;;
-;;
+
+;; -- "cond" is a procedure which evaluates the test (atom ? (car q)) and if this expression
+;; 		is true, which it is, the corresponding sequence after it (car q) is returned.
+
+;; -- the optional "else" clause is a procedure that is evaluated if none of the
+;; 		preceding tests yields a true value
 
