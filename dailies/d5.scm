2a6958bafa982dbfe1d93b4fb0300e31b74518fc
Matthew Phelps <mphelps3@nd.edu>
2019-01-26 13:54:40 -0500

    added daily 5

---
+++ dailies/d5.scm
@@ -0,0 +1,25 @@
+;; Matthew Phelps
+;; Jan 26 2019
+
+;; utility functions, finish these first!
+(define firsts
+
+(define member?
+
+(define subset?
+
+;; data variables
+(define dict  '((t o m a t o) (f i r e) (s a u c e) (c h i c k e n) (f r i e d) (f l a m e s)))
+(define words '((o i l p r e s s u r e) (o i l t e m p) (m a g n e t i c c o m p a s s) (s e a t b e l t s) (e l t) (a i r s p e e d) (a l t i m e t e r) (t a c h o m e t e r) (m a n i f o l d) (f u e l g a u g e) (t e m p g a u g e) (l a n d i n g g e a r l i g h t)))
+
+;; acronym finder
+(define acronyms
+;; your code here ;-)
+;; be sure to build your code using the utility functions from above
+
+(display (acronyms dict words))
+(display "\n")
+
+;; output should be:
+;; ((t o m a t o) (f l a m e s))
+
