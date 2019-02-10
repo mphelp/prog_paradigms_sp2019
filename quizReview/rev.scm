46f54baa3aefe2d508e2f0b889c6a36b0d1a247b
Matthew Phelps <mphelps3@nd.edu>
2019-02-10 17:19:56 -0500

    attempting occur*. adding paradigms_d1 for lat check

--- quizReview/rev.scm
+++ quizReview/rev.scm
@@ -1,10 +1,17 @@
+(load-from-path "/escnfs/home/cmc/public/paradigms/scheme/d1/paradigms_d1.scm")
+;;
+(use-modules (ice-9 paradigms_d3))
+
 ;; 1
-(define occur
-	(lambda (a lat)
+(define occur*
+	(lambda (a l)
 		(cond
-			((null? lat) 0)
-			((eq? a (car lat))(+ 1 (occur a (cdr lat))))
-			(else (occur a (cdr lat)))
+			((null? l) 0)
+			((lat? l)(cond
+					((null? l) 0)
+					((eq? a (car l))(+ 1 (occur* a (cdr l))))
+					(else (occur* a (cdr l)))))
+			(else + (occur* a (car l))(occur* a (cdr l)))
 )	)	)
 
 
@@ -15,7 +22,7 @@
 ;; Tests
 (define test1 
 	(cond
-		((eq? 2 (occur 'car '(car y y car u)))'(1 occur works))
-		(else '(1 occur fails))))
+		((eq? 2 (occur* 'car '(car y y car u)))'(1 occur* works))
+		(else '(1 occur* fails))))
 (display test1)
 (display "\n")
