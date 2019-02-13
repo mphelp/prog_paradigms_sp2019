d0987e9207bd644652fd10689c28d70a70bedb04
Matthew Phelps <mphelps3@nd.edu>
2019-02-13 15:52:15 -0500

    Progress

--- dailies/d9/re.cpp
+++ dailies/d9/re.cpp
@@ -2,12 +2,18 @@
 #include <boost/any.hpp>
 #include <vector>
 #include <string>
+#include <iostream>
 
 typedef std::vector<boost::any> Regex;
 bool match(std::string s, Regex re){
+	// If for each obj in Regex, check is true
+	// Then return true
+	// Else return false
 }
 int main(){
 	Regex re;
+	plus p("who");
+	std::cout << p.str << std::endl;
 
 	return 0;
 }
--- dailies/d9/re.h
+++ dailies/d9/re.h
@@ -1,9 +1,20 @@
-class plus {
+#include <string>
+typedef struct plus {
+	std::string str;
+	plus(){}
+	plus(std::string s){ this->s = s; }	
+} plus;
+typedef struct dot {
 	
-};
-class dot {
+} dot;
+typedef struct star {
 	
-};
-class star {
-	
-};
+} star; 
+
+
+bool check(plus& p, unsigned strIndex){
+}
+bool check(dot& p, unsigned strIndex){
+}
+bool check(star& p, unsigned strIndex){
+}
