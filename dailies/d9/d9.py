16c0a652c2b86f700f4b040cff4934bfecfa2e4a
Matthew Phelps <mphelps3@nd.edu>
2019-02-15 13:28:09 -0500

    python list by assignment issues

--- dailies/d9/d9.py
+++ dailies/d9/d9.py
@@ -5,14 +5,21 @@ MEMLIMIT = 10
 
 
 def join(newExpr, myre):
-    if not isinstance(myre, list) or not isinstance(newExpr, list):
+    if not isinstance(myre, list):
         print("ERROR: Expression parsed not a list")
+        return None
+    if not isinstance(newExpr, list):
+        print("ERROR: Myre parsed not a list")
+        return None
+    newMyre = []
     for option in newExpr:
         if len(myre) == 0:
-            myre.append(option)
+            newMyre.append(option)
         else:
             for regex in myre:
-                regex.append(option)
+                newRegex = regex + option
+                newMyre.append(newRegex)
+    return newMyre
 
 def plus(q):
     p = []
@@ -32,12 +39,18 @@ def dot():
     return [c for c in "abcdefghijklmnopqrstuvwxyz"]
 def add(expr, myre):
     if isinstance(expr, str):
-        join([expr], myre) 
+        newMyre = join([expr], myre) 
     else:
-        join(expr, myre) 
+        newMyre = join(expr, myre) 
+    return newMyre
 
+def idiot():
+    return ['p']
 if __name__ == '__main__':
     myre = []
-    add("a", myre)
-    add(star("b"), myre) 
+    myre = add("a", myre)
+    myre = add(star("b"), myre)
+    myre = add(dot(), myre)
     print(myre)
+
+
