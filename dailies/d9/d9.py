68b7d483105d545f4ad433ff7bce3bb3707cf970
Matthew Phelps <mphelps3@nd.edu>
2019-02-15 13:31:26 -0500

    first part of d9 finished

--- dailies/d9/d9.py
+++ dailies/d9/d9.py
@@ -3,7 +3,29 @@
 # Globals
 MEMLIMIT = 10
 
-
+# Functions
+def plus(q):
+    p = []
+    chars = ""
+    for i in range(MEMLIMIT):
+        chars += q
+        p.append(chars)
+    return p
+def star(q):
+    s = []
+    chars = ""
+    for i in range(MEMLIMIT):
+        s.append(chars)
+        chars += q
+    return s
+def dot():
+    return [c for c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+def add(expr, myre):
+    if isinstance(expr, str):
+        newMyre = join([expr], myre) 
+    else:
+        newMyre = join(expr, myre) 
+    return newMyre
 def join(newExpr, myre):
     if not isinstance(myre, list):
         print("ERROR: Expression parsed not a list")
@@ -20,37 +42,15 @@ def join(newExpr, myre):
                 newRegex = regex + option
                 newMyre.append(newRegex)
     return newMyre
+def match(str, myre):
+    return str in myre
 
-def plus(q):
-    p = []
-    chars = ""
-    for i in range(MEMLIMIT):
-        chars += q
-        p.append(chars)
-    return p
-def star(q):
-    s = []
-    chars = ""
-    for i in range(MEMLIMIT):
-        s.append(chars)
-        chars += q
-    return s
-def dot():
-    return [c for c in "abcdefghijklmnopqrstuvwxyz"]
-def add(expr, myre):
-    if isinstance(expr, str):
-        newMyre = join([expr], myre) 
-    else:
-        newMyre = join(expr, myre) 
-    return newMyre
-
-def idiot():
-    return ['p']
 if __name__ == '__main__':
     myre = []
-    myre = add("a", myre)
-    myre = add(star("b"), myre)
+    myre = add(plus("cat"), myre)
     myre = add(dot(), myre)
-    print(myre)
-
-
+    myre = add("d", myre)
+    myre = add("o", myre)
+    myre = add(star("g"), myre)
+    print(match("catcatQdogggggg", myre))
+    print(match("turkeybaconcheese", myre))
