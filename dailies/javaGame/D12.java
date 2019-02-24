5af71366f82f885fcdcbee860d29fefcb5363290
Matthew Phelps <mphelps3@nd.edu>
2019-02-24 15:59:43 -0500

    modified run

--- dailies/javaGame/D12.java
+++ dailies/javaGame/D12.java
@@ -1,6 +1,6 @@
 // Name: Matthew Phelps
-// Date: Feb 21 2019
-public class D12{
+// Date: Feb 24 2019
+public class PGame{
 	public static void main(String args[]){
 		MyGame mygame = new MyGame();
 		mygame.start();
