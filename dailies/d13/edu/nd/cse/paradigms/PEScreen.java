9efae6e46b43cf832b419f721e2387eb57d6e724
Matthew Phelps <mphelps3@nd.edu>
2019-02-27 13:26:52 -0500

    pixel setting progress in PEScreen

--- dailies/d13/edu/nd/cse/paradigms/PEScreen.java
+++ dailies/d13/edu/nd/cse/paradigms/PEScreen.java
@@ -16,15 +16,23 @@ public class PEScreen {
 
 		this.bg = 0x22BB11;
 
+		this.clear();
 	}
 	public void setPixel(int px, int py, int color){
 		pixels[px][py] = color;
 	}
-	public void clear(){}
+	public void clear(){
+		for (int i = 0; i < width; i++){
+			for (int j = 0; j < height; j++){
+				setPixel(i, j, bg);
+			}
+		}
+	}
 	public boolean inBounds(int px, int py){
-		return false;
+		return (py < height && py >= 0 && px < width && px >= 0);
 	}
 	public BufferedImage render(){
+		image.setRGB(0,0,width,height,pixels,1);
 		return image;
 	}
 }
