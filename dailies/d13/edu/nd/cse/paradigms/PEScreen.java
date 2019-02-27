d65cb241664c0add918074cb6b94c5b7ab029557
Matthew Phelps <mphelps3@nd.edu>
2019-02-27 13:05:58 -0500

    fixed screen default returns

--- dailies/d13/edu/nd/cse/paradigms/PEScreen.java
+++ dailies/d13/edu/nd/cse/paradigms/PEScreen.java
@@ -5,9 +5,15 @@ package edu.nd.cse.paradigms;
 import java.awt.image.BufferedImage;
 
 public class PEScreen {
+	private BufferedImage image;
+
 	public PEScreen(int width, int height){}
 	public void setPixel(int px, int py, int color){}
 	public void clear(){}
-	public boolean inBounds(int px, int py){}
-	public BufferedImage render(){}
+	public boolean inBounds(int px, int py){
+		return false;
+	}
+	public BufferedImage render(){
+		return image;
+	}
 }
