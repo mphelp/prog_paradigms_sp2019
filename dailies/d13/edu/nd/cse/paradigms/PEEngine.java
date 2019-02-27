ad06da27a4b99eb7cbd26774e6377b44d4752394
Matthew Phelps <mphelps3@nd.edu>
2019-02-27 11:20:56 -0500

    starting package

---
+++ dailies/d13/edu/nd/cse/paradigms/PEEngine.java
@@ -0,0 +1,12 @@
+// Name: Matthew Phelps
+// Date: Feb 27 2019
+
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class PEEngine extends Frame {
+	// protected PEGame game;
+	public PEEngine(){}
+	public void paint(Graphics g){}
+}
---
+++ dailies/d13/edu/nd/cse/paradigms/PEScreen.java
@@ -0,0 +1,11 @@
+package edu.nd.cse.paradigms;
+
+import java.awt.image.BufferedImage;
+
+public class PEScreen {
+	public PEScreen(int width, int height){}
+	public void setPixel(int px, int py, int color){}
+	public void clear(){}
+	public boolean inBounds(int px, int py){}
+	public BufferedImage render(){}
+}
