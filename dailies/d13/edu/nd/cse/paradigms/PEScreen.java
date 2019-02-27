00d926cd54d20dc6f5099b36a3ad9a837349e265
Matthew Phelps <mphelps3@nd.edu>
2019-02-27 13:12:13 -0500

    Add attr to PEScreen. Defined setPixel

--- dailies/d13/edu/nd/cse/paradigms/PEScreen.java
+++ dailies/d13/edu/nd/cse/paradigms/PEScreen.java
@@ -5,10 +5,21 @@ package edu.nd.cse.paradigms;
 import java.awt.image.BufferedImage;
 
 public class PEScreen {
+	private int width, height;
+	private int bg;
 	private BufferedImage image;
+	private int[][] pixels; // x then y
 
-	public PEScreen(int width, int height){}
-	public void setPixel(int px, int py, int color){}
+	public PEScreen(int width, int height){
+		this.width = width;
+		this.height = height;
+
+		this.bg = 0x22BB11;
+
+	}
+	public void setPixel(int px, int py, int color){
+		pixels[px][py] = color;
+	}
 	public void clear(){}
 	public boolean inBounds(int px, int py){
 		return false;
