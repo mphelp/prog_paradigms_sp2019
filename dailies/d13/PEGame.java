6406fa31559adca87656ee2bc7da133a1cd41b73
Matthew Phelps <mphelps3@nd.edu>
2019-02-27 11:02:28 -0500

    d13 start

---
+++ dailies/d13/PEGame.java
@@ -0,0 +1,12 @@
+// Name: Matthew Phelps
+// Date: Feb 27 2019
+
+import edu.nd.cse.paradigms.*;
+
+class MyGame extends PEGame {
+	private PEEngine engine;
+	public void start(){
+		engine = new PEEngine(this);
+	}
+}
+
