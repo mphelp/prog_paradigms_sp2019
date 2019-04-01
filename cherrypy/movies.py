c95d2fd0bc8a61f2434c108bdd75469676b89e7b
Matthew Phelps <mphelps3@nd.edu>
2019-03-31 21:35:59 -0400

    GET_MOVIE test

--- cherrypy/movies.py
+++ cherrypy/movies.py
@@ -8,6 +8,17 @@ class MoviesController:
     def GET_MOVIES(self):
         pass
 
-    def GET_MOVIE(self, movie_id)
+    def GET_MOVIE(self, movie_id):
         movie_id = int(movie_id)
+        title, genres = self.mdb.get_movie(movie_id)
+        if title is None or genres is None:
+            return json.dumps({ "result": "failure" })
+        else:
+            d = {
+                "genres": genres,
+                "title" : title,
+                "result": "success",
+                "id"    : movie_id
+            }
+            return json.dumps(d)
 
